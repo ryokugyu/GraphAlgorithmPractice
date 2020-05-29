859c0d4c596da0900243bbc8c96256b898f87c6a
Himanshu Nailwal <himanshunailwal.rhce@gmail.com>
2020-05-29 10:26:15 +0530

    Tree Implementation added

--- src/CycleDetectionUnionFind.java
+++ src/CycleDetectionUnionFind.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
+/*
+We will use union find algorithm alongside Disjoint sets for finding cycles in undirected graph.
+Assumptions - No self loops present.
+ */
+/*
 public class CycleDetectionUnionFind {
+    int vertex, E;
+    Edge edge[];
+
+    class Edge{
+        int src, dest;
+    }
+
+    CycleDetectionUnionFind(int v, int e){
+        vertex=v;
+        E=e;
+        edge = new Edge[E];
+        for(int i =0; i<e; i++){
+            edge[i] = new Edge[];
+        }
+    }
+
+
 }
+*/
--- src/TreeImplementation.java
+++ src/TreeImplementation.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+/*
+Creating a simple tree
+        ----
+        1    <-- root
+      /   \
+     2     3
+    /
+   4
+*/
+class Node{
+    int key;
+    Node left, right;
 
+    Node(int i){
+        key = i;
+        left = right = null;
+    }
+}
 public class TreeImplementation {
+    Node root;
+
+    //Binary Tree implementation -- Constructor
+    TreeImplementation(int key){
+        root = new Node(key);
+    }
+
+    TreeImplementation(){
+        root = null;
+    }
+
+    public static void main(String[] args){
+        TreeImplementation t = new TreeImplementation();
+
+        t.root = new Node(1);
+        t.root.left = new Node(2);
+        t.root.left.left = new Node(4);
+        t.root.right = new Node(3);
+    }
 }
